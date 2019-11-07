95a53d9a203d3521e137d041f147330290c068c3
Safwan25 <56427752+Safwan25@users.noreply.github.com>
2019-11-07 14:33:06 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?
+error_reporting(0);
+$host = "localhost"; 
+$username = "talukmas_won";  //USERNAME MYSQL
+$password = "Safwan25@";  //PASSWORD MYSQL
+$dbname = "talukmas_talukmasjid";  //DB NAME MYSQL
+
+$secret = '87143518-dccf-41ed-bb54-5ea91ec2befa'; //BEARERTOKENAKUNUTAMAA
+$pin = "251196"; //PINGOPAY
+$amount = rand(2,7);  //RANDOM NOMINAL
+$amountt = $1202;
+
+?>
